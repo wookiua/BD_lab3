aacf38a6324682b3d9a5a31e12e3385ccda6d1b2
Oleg <maolegtor@gmail.com>
2026-05-11 13:56:02 +0200

    clean

--- migrations/env.py
+++ migrations/env.py
@@ -10,7 +10,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Тепер імпортуємо Base, який містить опис усіх таблиць
+
 from models.database import Base
 
 
